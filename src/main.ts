0deb21c6e416f6abfdfdc9996b8a7719d8704a05
Precious2712 <oluwanifemimoonlight@gmail.com>
2025-10-02 10:19:39 -0800

    done

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: [
+      'http://localhost:3000', 
+      'http://localhost:3001', 
+      'https://bank-transfer-one.vercel.app', 
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
